bb30e2eb20fceb07645befa5456af91490a51e9d
Zain Wania <zain@wania.dev>
2023-01-27 09:34:49 -0500

    yo

--- app.config.ts
+++ app.config.ts
@@ -19,7 +19,7 @@ export default defineAppConfig({
     footer: {
       credits: {
         enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        repository: 'https://www.github.com/ZainW/zainwaniadev' // our github repository
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
