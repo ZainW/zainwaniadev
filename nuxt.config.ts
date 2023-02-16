6f7f99eff4e2c0ff07b6dc50b93606a1fbf0b068
Zain Wania <zain@wania.dev>
2023-02-15 20:10:30 -0500

    html attrs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,19 @@ export default defineNuxtConfig({
     preference: 'dark',
     fallback: 'dark',
     classSuffix: ''
+  },
+  app: {
+    head: {
+      htmlAttrs:{
+        lang: 'en'
+      },
+      charset: 'utf-16',
+      viewport: 'width=500, initial-scale=1',
+      title: 'Zain Wania',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'description', content: 'Personal Site' }
+      ],
+    }
   }
 })
